120b50f13cfd6654bc148a6bcff75aa3cba21b98
markwang0 <71631906+markwang0@users.noreply.github.com>
2022-12-09 21:41:08 -0600

    conda env

--- calc_all_inun.sh
+++ calc_all_inun.sh
@@ -2,7 +2,7 @@
 
 # run crop_inun.py in each HUC8 directory
 # HUC8 directories of interest begin with 12*
-# activate gis-env conda environment first
+# activate hand-austin-exercises conda environment first
 
 for D in 12*; do
     if [ -d "${D}" ]; then
