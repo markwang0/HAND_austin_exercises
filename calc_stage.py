d3228c0912b783401a4730c22699e2cfc0dc7d29
markwang0 <71631906+markwang0@users.noreply.github.com>
2022-12-17 15:51:19 -0600

    interp stages for full modules

--- calc_stage.py
+++ calc_stage.py
@@ -1,41 +1,72 @@
 import numpy as np
 import os
+import sys
 import pandas as pd
 
-# read in corrected streamflows for Llano HUC8 module 1
-mod1_comids_q_df = pd.read_excel(
-    "./AustinExerciseModules_rev/HAND_streams_output_TableToExcel.xlsx",
-    usecols=["HydroID", "DischargeCMS"],
-)
-mod1_comids_q_df.index = mod1_comids_q_df["HydroID"]
-mod1_comids_q_df = mod1_comids_q_df.drop(columns="HydroID")
+# read in corrected streamflows
 
-# read in synthetic rating curves for Llano HUC8 from Yan
-llano_hydrotable = pd.read_csv(
-    "12090204/hydroTable.csv",
+module1_df = pd.read_csv(
+    "./AustinExerciseModules_rev/module1.csv",
+    usecols=["HydroID", "streamflow"],
+)
+module1_df.index = module1_df["HydroID"]
+module1_df.drop(columns="HydroID", inplace=True)
+
+module2a_df = pd.read_csv(
+    "./AustinExerciseModules_rev/module2a.csv",
+    usecols=["HydroID", "streamflow"],
+)
+module2a_df.index = module2a_df["HydroID"]
+module2a_df.drop(columns="HydroID", inplace=True)
+
+module2b_df = pd.read_csv(
+    "./AustinExerciseModules_rev/module2b.csv",
+    usecols=["HydroID", "streamflow"],
+)
+module2b_df.index = module2b_df["HydroID"]
+module2b_df.drop(columns="HydroID", inplace=True)
+
+module3a_df = pd.read_csv(
+    "./AustinExerciseModules_rev/module3a.csv",
+    usecols=["HydroID", "streamflow"],
+)
+module3a_df.index = module3a_df["HydroID"]
+module3a_df.drop(columns="HydroID", inplace=True)
+
+module3b_df = pd.read_csv(
+    "./AustinExerciseModules_rev/module3b.csv",
+    usecols=["HydroID", "streamflow"],
+)
+module3b_df.index = module3b_df["HydroID"]
+module3b_df.drop(columns="HydroID", inplace=True)
+
+# read in master synthetic rating curves csv
+hydrotable = pd.read_csv(
+    "./AustinExerciseModules_rev/hydroTable_all.csv",
     usecols=["HydroID", "stage", "discharge_cms"],
 )
-llano_hydrotable.index = llano_hydrotable["HydroID"]
-llano_hydrotable = llano_hydrotable.drop(columns="HydroID")
+hydrotable.index = hydrotable["HydroID"]
+hydrotable = hydrotable.drop(columns="HydroID")
 
 # interpolate stage from streamflows with synthetic rating table
-mod1_comids_q_df["stage"] = np.nan
-for hydroid in mod1_comids_q_df.index:
-    # for each HydroID, subset hydrotable.csv
-    # to get the HydroID specific rating table
-    # and interpolate stage from discharge
-    stage = np.interp(
-        mod1_comids_q_df.loc[hydroid, "DischargeCMS"],
-        llano_hydrotable.loc[hydroid, "discharge_cms"],
-        llano_hydrotable.loc[hydroid, "stage"],
-    )
-    mod1_comids_q_df.loc[hydroid, "stage"] = stage
+mod_dfs = [module1_df, module2a_df, module2b_df, module3a_df, module3b_df]
+for (mod_df, name) in zip(
+    mod_dfs,
+    ["mod1", "mod2a", "mod2b", "mod3a", "mod3b"],
+):
 
-# save file with interpolated stage values and correct column names
-mod1_comids_q_df.index = mod1_comids_q_df.index.rename("FATSGTID")
-mod1_comids_q_df = mod1_comids_q_df.rename(
-    columns={"stage": "stage_m"},
-)
-mod1_comids_q_df.to_csv(
-    "12090204/mod1_rev.csv",
-)
+    mod_df["stage_m"] = np.nan
+    for hydroid in mod_df.index:
+        # for each HydroID, subset hydrotable.csv
+        # to get the HydroID specific rating table
+        # and interpolate stage from discharge
+        stage = np.interp(
+            mod_df.loc[hydroid, "streamflow"],
+            hydrotable.loc[hydroid, "discharge_cms"],
+            hydrotable.loc[hydroid, "stage_m"],
+        )
+        mod_df.loc[hydroid, "stage_m"] = stage
+
+    mod_df.to_csv(
+        "{name}_stage.csv",
+    )
