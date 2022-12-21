41900775608d50149ba3c39e2d6584725a11cc98
markwang0 <71631906+markwang0@users.noreply.github.com>
2022-12-21 15:19:53 -0600

    update for 20221221 flows

--- calc_inun.py
+++ calc_inun.py
@@ -4,11 +4,15 @@ import pandas as pd
 import rasterio as rio
 import sys
 
-module1_df = pd.read_csv("./AustinExerciseModules_rev/mod1_stage.csv")
-module2a_df = pd.read_csv("./AustinExerciseModules_rev/mod2a_stage.csv")
-module2b_df = pd.read_csv("./AustinExerciseModules_rev/mod2b_stage.csv")
-module3a_df = pd.read_csv("./AustinExerciseModules_rev/mod3a_stage.csv")
-module3b_df = pd.read_csv("./AustinExerciseModules_rev/mod3b_stage.csv")
+# pass HUC8 name (subdirectory name) as command line argument
+huc8_path = sys.argv[1]
+calc_id = sys.argv[2]
+
+module1_df = pd.read_csv(f"./AustinExerciseModules_rev/{calc_id}_mod1_stage.csv")
+module2a_df = pd.read_csv(f"./AustinExerciseModules_rev/{calc_id}_mod2a_stage.csv")
+module2b_df = pd.read_csv(f"./AustinExerciseModules_rev/{calc_id}_mod2b_stage.csv")
+module3a_df = pd.read_csv(f"./AustinExerciseModules_rev/{calc_id}_mod3a_stage.csv")
+module3b_df = pd.read_csv(f"./AustinExerciseModules_rev/{calc_id}_mod3b_stage.csv")
 
 mod_dfs_temp = [
     module1_df,
@@ -17,9 +21,6 @@ mod_dfs_temp = [
     module3a_df,
     module3b_df,
 ]
-
-# pass HUC8 name (subdirectory name) as command line argument
-huc8_path = sys.argv[1]
 
 catch_df = gpd.read_file(
     f"{huc8_path}/gw_catchments_reaches_filtered_addedAttributes_crosswalked.gpkg",
@@ -38,31 +39,25 @@ for (mod_df, name) in zip(
     for hydroid in mod_df["HydroID"]:
         if len(mod_df["stage_m"].loc[mod_df["HydroID"] == hydroid]) != 0:
             stage = mod_df["stage_m"].loc[mod_df["HydroID"] == hydroid].item()
-            for (fpath, heal_status) in zip(
-                ["hand_FATSGTID_", "hand_healed_FATSGTID_"],
-                ["unhealed", "healed"],
-            ):
+            with rio.open(
+                f"{huc8_path}/cropped_hand/hand_healed_FATSGTID_{hydroid}.tif"
+            ) as src:
+                hand = src.read(1, masked=True)
+                profile = src.profile
+            # set inundation to 0 for nodata (-9999) and hand >= stage
+            # otherwise inundation is stage minus hand
+            inun = np.where((hand >= stage) | (hand < 0), 0, stage - hand)
+            inun = np.ma.masked_where(inun == 0, inun)  # mask out 0 inundation
+            # save mask with GDAL convention
+            msk = (~inun.mask * 255).astype("uint8")
+            with rio.Env(GDAL_TIFF_INTERNAL_MASK=True):
                 with rio.open(
-                    f"{huc8_path}/cropped_hand/{fpath}{hydroid}.tif"
-                ) as src:
-                    hand = src.read(1, masked=True)
-                    profile = src.profile
-                # set inundation to 0 for nodata (-9999) and hand >= stage
-                # otherwise inundation is stage minus hand
-                inun = np.where((hand >= stage) | (hand < 0), 0, stage - hand)
-                inun = np.ma.masked_where(
-                    inun == 0, inun
-                )  # mask out 0 inundation
-                # save mask with GDAL convention
-                msk = (~inun.mask * 255).astype("uint8")
-                with rio.Env(GDAL_TIFF_INTERNAL_MASK=True):
-                    with rio.open(
-                        f"{huc8_path}/inundation_rev/{name}/{heal_status}_inun_{hydroid}.tif",
-                        "w",
-                        **profile,
-                    ) as ds_out:
-                        ds_out.write(inun, 1)
-                        if isinstance(msk, np.uint8):
-                            continue
-                        else:
-                            ds_out.write_mask(msk)
+                    f"{huc8_path}/inundation_{calc_id}/{name}/healed_inun_{hydroid}.tif",
+                    "w",
+                    **profile,
+                ) as ds_out:
+                    ds_out.write(inun, 1)
+                    if isinstance(msk, np.uint8):
+                        continue
+                    else:
+                        ds_out.write_mask(msk)
--- inun_launcher_prep.sh
+++ inun_launcher_prep.sh
@@ -7,9 +7,13 @@ if [ -f inun_launcher.sh ]; then
     rm inun_launcher.sh
 fi
 
+# calculation identifier, change each run
+# using flows sent from Tim on 2022-12-21
+export calc_id=20221221
+
 for D in 12*; do
     if [ -d "${D}" ]; then
-        mkdir -p ${D}/inundation_rev/mod{1,2a,2b,3a,3b}
-        echo "python calc_inun.py \"${D}\"" >> inun_launcher.sh
+        mkdir -p ${D}/inundation_${calc_id}/mod{1,2a,2b,3a,3b}
+        echo "python calc_inun.py \"${D}\" \"${calc_id}\"" >> inun_launcher.sh
     fi
 done
