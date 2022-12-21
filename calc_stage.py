ea156be186af9ca823b0341f34e110ad01741272
markwang0 <71631906+markwang0@users.noreply.github.com>
2022-12-21 14:54:29 -0600

    find stage for 20221221 flows from Tim

--- calc_stage.py
+++ calc_stage.py
@@ -5,57 +5,29 @@ import pandas as pd
 
 # read in corrected streamflows
 
-module1_df = pd.read_csv(
-    "./AustinExerciseModules_rev/module1.csv",
-    usecols=["HydroID", "streamflow"],
+df = pd.read_csv(
+    "./AustinExerciseModules_rev/20221221_FATSGTID_flows_all_modules.csv",
 )
-module1_df.index = module1_df["HydroID"]
-module1_df.drop(columns="HydroID", inplace=True)
 
-module2a_df = pd.read_csv(
-    "./AustinExerciseModules_rev/module2a.csv",
-    usecols=["HydroID", "streamflow"],
-)
-module2a_df.index = module2a_df["HydroID"]
-module2a_df.drop(columns="HydroID", inplace=True)
-
-module2b_df = pd.read_csv(
-    "./AustinExerciseModules_rev/module2b.csv",
-    usecols=["HydroID", "streamflow"],
-)
-module2b_df.index = module2b_df["HydroID"]
-module2b_df.drop(columns="HydroID", inplace=True)
-
-module3a_df = pd.read_csv(
-    "./AustinExerciseModules_rev/module3a.csv",
-    usecols=["HydroID", "streamflow"],
-)
-module3a_df.index = module3a_df["HydroID"]
-module3a_df.drop(columns="HydroID", inplace=True)
-
-module3b_df = pd.read_csv(
-    "./AustinExerciseModules_rev/module3b.csv",
-    usecols=["HydroID", "streamflow"],
-)
-module3b_df.index = module3b_df["HydroID"]
-module3b_df.drop(columns="HydroID", inplace=True)
+df.index = df["FATSGTID"]
+df.drop(columns="FATSGTID", inplace=True)
 
 # read in master synthetic rating curves csv
 hydrotable = pd.read_csv(
-    "./AustinExerciseModules_rev/hydroTable_all.csv",
-    usecols=["HydroID", "stage", "discharge_cms"],
+    "./AustinExerciseModules_rev/hydroTable_all_rp_bf_lmtdischarge_cda.csv",
+    usecols=["FATSGTID", "stage", "discharge_cms"],
 )
-hydrotable.index = hydrotable["HydroID"]
-hydrotable = hydrotable.drop(columns="HydroID")
+hydrotable.index = hydrotable["FATSGTID"]
+hydrotable.drop(columns="FATSGTID", inplace=True)
 
 # interpolate stage from streamflows with synthetic rating table
-mod_dfs = [module1_df, module2a_df, module2b_df, module3a_df, module3b_df]
-for (mod_df, name) in zip(
-    mod_dfs,
-    ["mod1", "mod2a", "mod2b", "mod3a", "mod3b"],
-):
+for name in ["1", "2a", "2b", "3a", "3b"]:
 
+    mod_df = pd.DataFrame()
+    mod_df.index = df.index
+    mod_df["streamflow"] = df[f"module{name}_cms"]
     mod_df["stage_m"] = np.nan
+
     for hydroid in mod_df.index:
         # for each HydroID, subset hydrotable.csv
         # to get the HydroID specific rating table
@@ -68,5 +40,7 @@ for (mod_df, name) in zip(
         mod_df.loc[hydroid, "stage_m"] = stage
 
     mod_df.to_csv(
-        f"./AustinExerciseModules_rev/{name}_stage.csv",
+        f"./AustinExerciseModules_rev/mod{name}_stage.csv",
     )
+
+    del mod_df
